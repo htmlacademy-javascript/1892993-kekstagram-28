8844d4c222c8e684ab4aced9b28e2474573f52d7
Sergey <serbog230797@gmail.com>
2023-03-31 22:50:37 +0300

    домашка

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { getData } from './api.js';
 import { initThumbnails } from './thumbnails.js';
+import { initUploadImage } from './upload-image.js';
 
 const data = await getData();
 
+initUploadImage();
 initThumbnails(data);
